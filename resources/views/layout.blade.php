d2ff1726698280ec6683b609bf11022e8959c8b4
Kene Arionye <kenechiaugustine@gmail.com>
2022-07-10 11:15:41 +0100

    changed footer styling

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -63,10 +63,10 @@
         @yield('content')
     </main>
     <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
+        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-20 mt-24 opacity-90 md:justify-center">
         <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
-        <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
+        <a href="/listings/create" class="absolute top-1/3 right-10 bg-black rounded-lg text-white py-2 px-5">Post Job</a>
     </footer>
 
     <x-flash-message />
