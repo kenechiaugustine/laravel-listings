9108f558bb0ccc8f1dcbf741b06e7bcfa9eaff2d
Kene Arionye <kenechiaugustine@gmail.com>
2022-08-19 06:36:52 +0100

    optimized for seo

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
 
+@section('head')
+    <title>Register</title>
+@endsection
+
 @section('content')
     <x-card class="p-10 max-w-lg mx-auto mt-24">
         <header class="text-center">
