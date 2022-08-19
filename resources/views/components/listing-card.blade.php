cf22c3f5310172b85975c4dd45fe456d11d7d095
Kene Arionye <kenechiaugustine@gmail.com>
2022-08-19 06:54:20 +0100

    updated alt text

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -4,7 +4,7 @@
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block"
             src={{ $listing->logo ? asset('storage/' . $listing->logo) : asset('images/no-image.png') }}
-            alt="" />
+            alt="Company Logo" />
         <div>
             <h3 class="text-2xl">
                 <a href="{{ $listing['id'] }}">{{ $listing['title'] }}</a>
