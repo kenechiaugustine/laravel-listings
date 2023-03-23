8a2519973b760cb6541615cbe22d3e72daea053e
Kenechukwu Arionye <kenechiaugustine@gmail.com>
2023-03-23 13:15:16 +0100

    modified footer date

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -65,7 +65,7 @@
     </main>
     <footer
         class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-20 mt-24 opacity-90 md:justify-center">
-        <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+        <p class="ml-2">Copyright &copy; <?=date('Y') ?> All Rights reserved</p>
 
         <a href="/listings/create" class="absolute top-1/3 right-10 bg-black rounded-lg text-white py-2 px-5">Post
             Job</a>
